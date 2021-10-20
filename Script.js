d1b83d4a12015086894787e54533677fffdfc786
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-10-20 09:51:41 +0700

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+Object.values(webpackJsonp.push([[],{[''] :(_,e,r)=>{e.cache=r.c}},
+[['']]]).cache).find(m=>m.exports&&m.exports.default&&m.exports.default.getCurrentUser!==void
+0).exports.default.getCurrentUser().premiumType = 2
+/* 2 = Nitro Boost
+   1 = Nitro Classic
+   0 = Normal
+*/
