d0eee101c692541fa37d01f25e2a1437e91716c9
Tung <89498812+tungdo0602@users.noreply.github.com>
2021-11-08 20:39:54 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().premiumType = 2;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().premiumType = 2;}}}]);console.clear();console.log(`%cNitro Perk Has Successfully enabled on your client!`, "font-size: 32px; color: #00ffff")
+/* Thank An00nymushun for the emoji part :) (his github go stars: https://github.com/An00nymushun/DiscordFreeEmojis) */
 (function() {
 
 'use strict';
